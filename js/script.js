80213662c54a601ccef9f9a66c3663d11303d3bb
alvaroph <alvaroph@gmail.com>
2025-09-23 09:58:32 +0200

    delegaacio d'events

--- js/script.js
+++ js/script.js
@@ -11,7 +11,6 @@ function actualitzaMarcador() {
     let htmlString= `Preguntes respostes ${estatDeLaPartida.contadorPreguntes}/${NPREGUNTAS} <br>`
     for (let i=0; i< estatDeLaPartida.respostesUsuari.length ; i++)
     {
-       // htmlString+="Pregunta" + i +":" +"<span class='badge text-bg-primary'>" +  (estatDeLaPartida.respostesUsuari[i]==undefined?"O":"X") + "</span>" + "<br>" 
         htmlString+= `Pregunta  ${i} : <span class='badge text-bg-primary'> 
                             ${(estatDeLaPartida.respostesUsuari[i]==undefined?"O":"X")}
                             </span><br>` 
@@ -20,7 +19,7 @@ function actualitzaMarcador() {
     marcador.innerHTML =htmlString;
    
 }
-
+//AQUESTA FUNCIO REACCCIONA A QUAN UN USUARI MARCA UN BOTO (PREGUNTA I RESPOSTA)
 function marcarRespuesta(numPregunta, numRespuesta) {
     console.log("Pregunta " + numPregunta + " Resposta " + numRespuesta);
    
@@ -40,23 +39,74 @@ function imprimirJuego(data){
 
         let htmlString="";
 
-    
+     
     for (let i=0; i<NPREGUNTAS; i++) {
             htmlString+=`<h3> ${data.preguntes[i].pregunta} </h3> `;
             htmlString+=`<img src="img/${data.preguntes[i].imatge}" alt="imatge pregunta ${i+1}"> <br>`;
 
             for (let j=0; j < data.preguntes[i].respostes.length ; j++){
-                htmlString+=`<button class="btn btn-primary" onclick="marcarRespuesta(${i},${j})"> 
+                htmlString+=`<button preg="${i}" resp="${j}" class="btn btn-primary"> 
                                 ${data.preguntes[i].respostes[j].resposta} 
                             </button> `;
             }
         }
 
-        htmlString+=`<button id="btnEnviar" onclick="enviarEstat()" class="btn btn-danger"  style="display:none" >Enviar Respuestas</button>`
+       //DELEGACIO D'EVENTS
+       //FEM QUE ESCOLTI EL PARE I DESPRES AMB UN IF VEIEM SI ENS INTERESA QUI HA FET CLICK
+       //DESPRES RECUPEREM LA INFORMACIO DEL e.target 
+        contenidor.addEventListener('click', function(e) {
+          console.log("han clickado a " + e.target)
+          if(e.target.classList.contains('btn')){
+             console.log("esto si es un boton que tiene los datos" + e.target.getAttribute("preg") +"--"+ e.target.getAttribute("resp"))
+              marcarRespuesta(e.target.getAttribute("preg"),e.target.getAttribute("resp"))
+            }
+          });
+
+        htmlString+=`<button id="btnEnviar"  class="btn btn-danger"  style="display:none" >Enviar Respuestas</button>`
             
         contenidor.innerHTML=htmlString;
-}
 
+      document.getElementById("btnEnviar").addEventListener("click", function(){
+         const url = "recollida.php"; // cambia por tu endpoint
+            // 1) Enviar como JSON
+            fetch(url, {
+              method: "POST",
+              headers: { "Content-Type": "application/json" },
+              body: JSON.stringify({
+                contadorPreguntes: estatDeLaPartida.contadorPreguntes,
+                respostesUsuari: estatDeLaPartida.respostesUsuari
+              })
+            })
+            .then(res => res.text())
+            .then(data => console.log("JSON ->", data));
+
+            // 2) Enviar como FormData (simulando formulario multipart)
+            let formData = new FormData();
+            formData.append("contadorPreguntes", estatDeLaPartida.contadorPreguntes);
+            formData.append("respostesUsuari", JSON.stringify(estatDeLaPartida.respostesUsuari));
+
+            fetch(url, {
+              method: "POST",
+              body: formData
+            })
+            .then(res => res.text())
+            .then(data => console.log("FormData ->", data));
+
+            // 3) Enviar como x-www-form-urlencoded
+            let params = new URLSearchParams();
+            params.append("contadorPreguntes", estatDeLaPartida.contadorPreguntes);
+            params.append("respostesUsuari", JSON.stringify(estatDeLaPartida.respostesUsuari));
+
+            fetch(url, {
+              method: "POST",
+              headers: { "Content-Type": "application/x-www-form-urlencoded" },
+              body: params
+            })
+            .then(res => res.text())
+            .then(data => console.log("URLEncoded ->", data));
+      })
+
+ }
 
 window.addEventListener('DOMContentLoaded', (event) => {
 
@@ -66,45 +116,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
        
 });
-  
+
 
 function enviarEstat() {
-  const url = "https://aperezh.daw.inspedralbes.cat/joc/recollida.php"; // cambia por tu endpoint
-
-  // 1) Enviar como JSON
-  fetch(url, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      contadorPreguntes: estatDeLaPartida.contadorPreguntes,
-      respostesUsuari: estatDeLaPartida.respostesUsuari
-    })
-  })
-  .then(res => res.text())
-  .then(data => console.log("JSON ->", data));
-
-  // 2) Enviar como FormData (simulando formulario multipart)
-  let formData = new FormData();
-  formData.append("contadorPreguntes", estatDeLaPartida.contadorPreguntes);
-  formData.append("respostesUsuari", JSON.stringify(estatDeLaPartida.respostesUsuari));
-
-  fetch(url, {
-    method: "POST",
-    body: formData
-  })
-  .then(res => res.text())
-  .then(data => console.log("FormData ->", data));
-
-  // 3) Enviar como x-www-form-urlencoded
-  let params = new URLSearchParams();
-  params.append("contadorPreguntes", estatDeLaPartida.contadorPreguntes);
-  params.append("respostesUsuari", JSON.stringify(estatDeLaPartida.respostesUsuari));
-
-  fetch(url, {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: params
-  })
-  .then(res => res.text())
-  .then(data => console.log("URLEncoded ->", data));
+ 
 }
